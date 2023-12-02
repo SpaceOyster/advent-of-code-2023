315c7ac665e0a6a55b7b1cb4aa9db6f069ea9660
SpaceOyster <spaceoyster@disroot.org>
2023-12-02 08:43:12 +0500

    added some type signatures and a few comments

--- src/Day/One.hs
+++ src/Day/One.hs
@@ -9,9 +9,9 @@ import qualified Data.Text.IO as T
 solution1 :: FilePath -> IO ()
 solution1 file = do
   contents <- T.readFile file
-  let ls = filter (/= "") $ T.splitOn "\n" contents
-      digits = fmap (T.filter isDigit) ls
-      calibrations = fmap (\t -> read [T.head t, T.last t] :: Integer) digits
+  let ls = filter (/= "") $ T.splitOn "\n" contents :: [T.Text]
+      digits = fmap (T.filter isDigit) ls :: [T.Text]
+      calibrations = fmap (\t -> read [T.head t, T.last t] :: Integer) digits :: [Integer]
       answer = sum calibrations
   print answer
 
@@ -28,10 +28,13 @@ substs =
     ("nine", "9")
   ]
 
+-- non optimal: we don't need to make all the substitutions, wee only need to
+-- scan for the first and the last
 parseWords :: T.Text -> Integer
 parseWords line =
   (\t -> read [T.head t, T.last t] :: Integer) $ T.filter isDigit $ ping line
   where
+    ping :: T.Text -> T.Text
     ping "" = ""
     ping l =
       case pong l of
@@ -41,14 +44,14 @@ parseWords line =
     pong "" = []
     pong l = do
       (n, r) <- substs
-      case T.stripPrefix n l of
-        Just _ -> pure r
-        Nothing -> []
+      case T.stripPrefix n l of -- stripPrefix automatically checks if n is prefiex of l
+        Just _ -> pure r -- when we get Just we know that n really was a prefix of l
+        Nothing -> [] -- when we get Nothing, well... n wasn't a prefix of l
 
 solution2 :: FilePath -> IO ()
 solution2 file = do
   contents <- T.readFile file
-  let ls = filter (/= "") $ T.splitOn "\n" contents
+  let ls = filter (/= "") $ T.splitOn "\n" contents :: [T.Text]
       calibrations = parseWords <$> ls
       answer = sum calibrations
   print answer
