4448f26ee91b03b9b616015348f61c3dea3bdbdb
SpaceOyster <spaceoyster@disroot.org>
2023-12-02 12:32:00 +0500

    added solution for first puzzle of Day Two

---
+++ src/Day/Two.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day.Two (solution1, solution2) where
+
+import Data.Maybe (fromMaybe, maybeToList)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+newtype CubesNumber = CubesNumber {getCubesNumber :: Integer}
+  deriving (Num, Eq, Ord, Enum, Show, Real, Integral)
+
+data SetOfCubes = SetOfCubes
+  { socRed :: CubesNumber,
+    socGreen :: CubesNumber,
+    socBlue :: CubesNumber
+  }
+  deriving (Eq, Show)
+
+instance Ord SetOfCubes where
+  a <= b = (socRed a <= socRed b) && (socGreen a <= socGreen b) && (socBlue a <= socBlue b)
+
+instance Semigroup SetOfCubes where
+  a <> b =
+    SetOfCubes
+      { socRed = socRed a + socRed b,
+        socGreen = socGreen a + socGreen b,
+        socBlue = socBlue a + socBlue b
+      }
+
+type Turn = SetOfCubes
+
+data Game = Game
+  { gNumber :: Integer,
+    gSequence :: [Turn]
+  }
+  deriving (Show)
+
+isGamePossible :: Game -> SetOfCubes -> Bool
+isGamePossible game setOfCubes = all (< setOfCubes) $ gSequence game
+
+findPossible :: [Game] -> SetOfCubes -> [Game]
+findPossible game setOfCubes = [g | g <- game, g `isGamePossible` setOfCubes]
+
+findImpossible :: [Game] -> SetOfCubes -> [Game]
+findImpossible game setOfCubes = [g | g <- game, not (g `isGamePossible` setOfCubes)]
+
+-- it's tempting to use parsec here
+parseGames :: T.Text -> [Game]
+parseGames text = do
+  line <- T.lines text
+  maybeToList $ parseGame line
+
+parseGame :: T.Text -> Maybe Game
+parseGame line = do
+  let (gameNumS, gameStr') = T.breakOn ": " line
+  gNumber <- parseGameNumber gameNumS
+  gameStr <- T.stripPrefix ": " gameStr'
+  gSequence <- traverse parseATurn $ T.splitOn "; " gameStr
+  pure $ Game {gNumber, gSequence}
+
+parseATurn :: T.Text -> Maybe Turn
+parseATurn = mconcat . fmap parseSubturn . T.splitOn ", "
+
+colors :: [T.Text]
+colors = ["red", "green", "blue"]
+
+parseSubturn :: T.Text -> Maybe SetOfCubes
+parseSubturn sub =
+  let xs = fmap (\x -> CubesNumber . read . T.unpack <$> T.stripSuffix (" " <> x) sub) colors
+   in case xs of
+        [r, g, b] -> Just $ SetOfCubes (fromMaybe 0 r) (fromMaybe 0 g) (fromMaybe 0 b)
+        _ -> Nothing
+
+parseGameNumber :: T.Text -> Maybe Integer
+parseGameNumber str = do
+  x <- T.stripPrefix "Game " str
+  pure $ read $ T.unpack x
+
+solution1 :: FilePath -> IO ()
+solution1 file = do
+  contents <- T.readFile file
+  let games = parseGames contents
+      setOfCubes = SetOfCubes {socRed = 12, socGreen = 13, socBlue = 14}
+  print $ sum (gNumber <$> findPossible games setOfCubes)
+
+solution2 file = do
+  contents <- T.readFile file
+  print "not implemented yet"
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import qualified Day.One
+import qualified Day.Two
 import qualified System.Environment as E
 
 main :: IO ()
@@ -9,4 +10,6 @@ main = do
   case day of
     "1.1" -> Day.One.solution1 inputFile
     "1.2" -> Day.One.solution2 inputFile
+    "2.1" -> Day.Two.solution1 inputFile
+    "2.2" -> Day.Two.solution2 inputFile
     _ -> print $ "no solution with index " <> show day
