28fbd676280a858d1b06db38413cd7a34e105db5
SpaceOyster <spaceoyster@disroot.org>
2023-12-05 19:43:31 +0500

    solved part two of Day Five

--- src/Day/Five.hs
+++ src/Day/Five.hs
@@ -5,7 +5,7 @@ module Day.Five (solution1, solution2) where
 
 import Control.Applicative ((<|>))
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, maybeToList)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
@@ -95,7 +95,74 @@ solution1 file = do
       seedLocations = followAlmanac almanac <$> aSeeds almanac
   print $ minimum seedLocations
 
+reinterpreteSeedList :: [Int] -> [Range]
+reinterpreteSeedList [] = []
+reinterpreteSeedList [_] = []
+reinterpreteSeedList (b : n : ss) = Range {rBegin = b, rEnd = b + n - 1} : reinterpreteSeedList ss
+
+rangeLeftDiff :: Range -> Range -> [Range]
+rangeLeftDiff l r
+  | all (`inRange` r) [bL, eL] = []
+  | all (< bL) [eR, bR] || all (> eL) [eR, bR] = pure l
+  | (bR > bL) && (eR < eL) = [Range bL (bR - 1), Range (eR + 1) eL]
+  | otherwise =
+      pure $
+        Range
+          (if bL `inRange` r then eR + 1 else bL)
+          (if eL `inRange` r then bR - 1 else eL)
+  where
+    bL = rBegin l
+    eL = rEnd l
+    bR = rBegin r
+    eR = rEnd r
+
+moveRange :: Int -> Range -> Range
+moveRange i Range {rBegin, rEnd} = Range (rBegin + i) (rEnd + i)
+
+rangeIntersection :: Range -> Range -> Maybe Range
+rangeIntersection l r =
+  if rEnd l >= rBegin r && rBegin l <= rEnd r
+    then Just $ Range {rBegin = rBegin l `max` rBegin r, rEnd = rEnd l `min` rEnd r}
+    else Nothing
+
+diffWithList :: Range -> [Range] -> [Range]
+diffWithList r = foldr f [r]
+  where
+    f a acc = mconcat . fromMaybe [] $ traverse (g a) acc
+    g a x = let o = rangeLeftDiff x a in if null o then Nothing else pure o
+
+mapRangeIntersectionFrom :: Range -> Sources -> Destinations -> Maybe Range
+mapRangeIntersectionFrom r s d = do
+  intersection <- rangeIntersection r s
+  let distance = rBegin d - rBegin s
+  pure $ moveRange distance intersection
+
+rangeFollowMap :: AlmanacMap -> Range -> [Range]
+rangeFollowMap m r =
+  mconcat $ unmatched : matched
+  where
+    matched =
+      Map.foldrWithKey (\s d acc -> maybeToList (mapRangeIntersectionFrom r s d) : acc) [] m
+    unmatched = diffWithList r (Map.keys m)
+
+followAlmanac2 :: Almanac -> [Range]
+followAlmanac2 Almanac {..} = do
+  range <- reinterpreteSeedList aSeeds
+  soil <- rangeFollowMap aSeedToSoil range
+  fertilizer <- rangeFollowMap aSoilToFertilizer soil
+  water <- rangeFollowMap aFertilizerToWater fertilizer
+  light <- rangeFollowMap aWaterToLight water
+  temperature <- rangeFollowMap aLightToTemperature light
+  humidity <- rangeFollowMap aTemperatureToHumidity temperature
+  rangeFollowMap aHumidityToLocation humidity
+
+minInRanges :: [Range] -> Int
+minInRanges = minimum . fmap rBegin
+
 solution2 :: FilePath -> IO ()
 solution2 file = do
   contents <- T.readFile file
-  print "unimplemented"
+  let almanac = fromMaybe emptyAlmanac $ parseAlmanac contents
+      locationRanges = followAlmanac2 almanac
+  print almanac
+  print $ minInRanges locationRanges
