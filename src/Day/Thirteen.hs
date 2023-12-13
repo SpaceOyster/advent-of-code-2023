08de520953d0f78d64588e90d3cc8eb3626847a6
SpaceOyster <spaceoyster@disroot.org>
2023-12-13 17:46:12 +0500

    solved part two of Day Thirteen

--- src/Day/Thirteen.hs
+++ src/Day/Thirteen.hs
@@ -2,8 +2,10 @@
 
 module Day.Thirteen (solution1, solution2) where
 
+import Control.Applicative ((<|>))
 import Control.Monad (guard)
-import Data.List (intercalate, transpose)
+import Data.List (elemIndices, intercalate, transpose)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.String (IsString (..))
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -37,25 +39,7 @@ newtype Pattern = Pattern {unPattern :: [TileRow]}
 instance Show Pattern where
   show p = "\nPattern: [\n" <> (intercalate "\n" . fmap show $ unPattern p) <> "\n]\n"
 
-transposePattern :: Pattern -> Pattern
-transposePattern = Pattern . transpose . unPattern
-
-findRepeatingRows :: Pattern -> [(Int, Int)]
-findRepeatingRows (Pattern p) = do
-  (idx, curr, next) <- zip3 [0 ..] p (drop 1 p)
-  guard (curr == next)
-  pure (idx, idx + 1)
-
-isRowPairReflective :: Pattern -> (Int, Int) -> Bool
-isRowPairReflective (Pattern p) (_a, b) =
-  let (pa, pb) = splitAt b p
-   in and $ zipWith (==) (reverse pa) pb
-
-findReflectiveRows :: Pattern -> [(Int, Int)]
-findReflectiveRows p = filter (isRowPairReflective p) $ findRepeatingRows p
-
-findReflectiveCols :: Pattern -> [(Int, Int)]
-findReflectiveCols = findReflectiveRows . transposePattern
+type RowIdx = Int
 
 parseRow :: T.Text -> TileRow
 parseRow = fromString . T.unpack
@@ -66,6 +50,26 @@ parsePattern = Pattern . fmap parseRow . T.lines
 parsePatterns :: T.Text -> [Pattern]
 parsePatterns = fmap parsePattern . T.splitOn "\n\n"
 
+transposePattern :: Pattern -> Pattern
+transposePattern = Pattern . transpose . unPattern
+
+findRepeatingRows :: Pattern -> [(RowIdx, RowIdx)]
+findRepeatingRows (Pattern p) = do
+  (idx, curr, next) <- zip3 [0 ..] p (drop 1 p)
+  guard (curr == next)
+  pure (idx, idx + 1)
+
+isRowPairReflective :: Pattern -> (RowIdx, RowIdx) -> Bool
+isRowPairReflective (Pattern p) (_a, b) =
+  let (pa, pb) = splitAt b p
+   in and $ zipWith (==) (reverse pa) pb
+
+findReflectiveRows :: Pattern -> [(RowIdx, RowIdx)]
+findReflectiveRows p = filter (isRowPairReflective p) $ findRepeatingRows p
+
+findReflectiveCols :: Pattern -> [(RowIdx, RowIdx)]
+findReflectiveCols = findReflectiveRows . transposePattern
+
 solution1 :: FilePath -> IO ()
 solution1 file = do
   contents <- T.readFile file
@@ -75,7 +79,31 @@ solution1 file = do
       answer = sum (fmap (sum . fmap snd) reflectiveCols) + 100 * sum (fmap (sum . fmap snd) reflectiveRows)
   print answer
 
+countDifferentTiles :: TileRow -> TileRow -> Int
+countDifferentTiles ra rb =
+  let eqs = zipWith (==) ra rb
+      idxs = elemIndices False eqs
+   in length idxs
+
+findReflRowWSmudge :: Pattern -> Maybe (RowIdx, RowIdx)
+findReflRowWSmudge (Pattern p) = listToMaybe $ do
+  idx <- [0 .. length p]
+  let (pa, pb) = splitAt (idx + 1) p
+      difference = sum $ zipWith countDifferentTiles (reverse pa) pb
+  guard (difference == 1)
+  pure (idx, idx + 1)
+
+findReflColWSmudge :: Pattern -> Maybe (RowIdx, RowIdx)
+findReflColWSmudge = findReflRowWSmudge . transposePattern
+
+sumMirrors :: Pattern -> Int
+sumMirrors p = fromMaybe 0 (fmap ((* 100) . snd) (findReflRowWSmudge p) <|> fmap snd (findReflColWSmudge p))
+
 solution2 :: FilePath -> IO ()
 solution2 file = do
   contents <- T.readFile file
-  print "unimplemented"
+  let patterns = parsePatterns contents
+      mirrorsNumbers = sumMirrors <$> patterns
+      answer = sum mirrorsNumbers
+  print mirrorsNumbers
+  print answer
